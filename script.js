87a73a72f3da0e64ad3fc2a044f308a3f568b831
timurtaua12-bit <timurtaua12@gmail.com>
2026-07-06 01:20:56 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("Aerospace Engineering research website loaded");
+function showTimelineFact() {
+  const facts = [
+    "The Wright Flyer flew for only 12 seconds in 1903.",
+    "Apollo 11 traveled about 240,000 miles to reach the Moon.",
+    "The Bell X-1 was the first aircraft to break the sound barrier.",
+    "Modern reusable rockets can land and be launched again."
+  ];
+
+  const randomFact = facts[Math.floor(Math.random() * facts.length)];
+  document.getElementById("timelineFact").textContent = randomFact;
+}
+
